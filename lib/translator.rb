ecc86fb273a6940cc9330377a4b099418822c2ae
Hemesh Patel <hemesh.v.patel@gmail.com>
2019-07-25 06:02:21 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,6 +6,13 @@ def load_library(file_path)
   
   new_hash = {"get_meaning" => {}, "get_emoticon" => {}}
   
+  file_path.each do |meaning, translation|
+    english = translation[0] #first part of Array of emoticons
+    japanese = translation[1] #second " "
+    new_hash["get_meaning"][japanese] = meaning
+    new_hash["get_emoticon"][english] = japanese
+  end
+  return new_hash
 end
 
 def get_japanese_emoticon
