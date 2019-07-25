0d44adffc305885b89fb81241b016177199a93b3
Hemesh Patel <hemesh.v.patel@gmail.com>
2019-07-25 06:11:58 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -17,11 +17,11 @@ end
 
 def get_japanese_emoticon(file_path, emoticon)
   library = load_library(file_path)
-  library["get_emoticon"].each do [key, value]
-    if key == emoticon
-      puts value
-    end
-  end
+  #library["get_emoticon"].each do [key, value]
+   # if key == emoticon
+    #  value
+    #end
+  #end
 end
 
 def get_english_meaning
