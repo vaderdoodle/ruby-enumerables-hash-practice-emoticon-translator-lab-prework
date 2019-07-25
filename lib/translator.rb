ba05a13dd498c3e5ba83f042aa76d3fc2f90510d
Hemesh Patel <hemesh.v.patel@gmail.com>
2019-07-25 06:17:45 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -27,7 +27,7 @@ def get_japanese_emoticon(file_path, emoticon)
   end
 end
 
-def get_english_meaning(file_path, meaning)
+def get_english_meaning(file_path, emoticon)
   library = load_library(file_path)
   
   result = library["get_meaning"][emoticon]
